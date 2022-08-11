9a75244d515db4bf524e1736743752eb5ec43059
iamrosada <luisrosada2018@gmail.com>
2022-08-11 19:55:16 +0300

    created the stateContext with useState

--- context/StateContext.js
+++ context/StateContext.js
@@ -1,2 +1,12 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { toast } from "react-hot-toast";
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+    const [show, setShow] = useState(false);
+    const [cartItems, setCartItems] = useState();
+    const [totalPrice, setTotalPrice] = useState();
+    const [totalQuantities, setTotalQuantities] = useState();
+    const [qty, setQty] = useState(1);
+};
