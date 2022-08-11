98ffaf82580af4cadc9e985213c7e7956cbd5fd3
iamrosada <luisrosada2018@gmail.com>
2022-08-11 15:37:12 +0300

    feature: created the Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
 import React from "react";
+import { AiOutlineShopping } from "react-icons/ai";
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    return (
+        <div className="navbar-container">
+            <p className="logo">
+                <Link href="/">Môbiolo Headphones</Link>
+            </p>
+            <button className="cart-icon" type="button">
+                <AiOutlineShopping />
+                <span className="cart-item-qty">1</span>
+            </button>
+        </div>
+    );
 };
 
 export default Navbar;
