11100de028e9973d3d8fba47f0ecbe0fab450c9b
iamrosada <luisrosada2018@gmail.com>
2022-08-11 16:01:03 +0300

    feature: added the getStaticProps

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,7 +1,31 @@
 import React from "react";
+import { urlFor, client } from "../../lib/client";
 
-const ProductDetails = () => {
-    return <div>ProductDetails</div>;
+const ProductDetails = ({ product, products }) => {
+    return (
+        <div>
+            <div className="product-detail-container">
+                <div>
+                    <div className="image-container">
+                        <img src={urlFor()} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
+export const getStaticProps = async ({ params: { slug } }) => {
+    const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+    const productQuery = '*[_type == "product"]';
 
+    const product = await client.fetch(query);
+    const products = await client.fetch(productQuery);
+
+    const bannerQuery = '*[_type == "banner"]';
+    const bannerData = await client.fetch(bannerQuery);
+
+    return {
+        props: { product, products },
+    };
+};
 export default ProductDetails;
