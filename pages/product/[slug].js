609ad27f9e039153007f6d00a74ef96af77bfcbf
iamrosada <luisrosada2018@gmail.com>
2022-08-11 19:25:37 +0300

    feature: added div for may also like product with track

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     AiOutlineMinus,
     AiOutlinePlus,
@@ -6,6 +6,7 @@ import {
     AiOutlineStar,
 } from "react-icons/ai";
 import { urlFor, client } from "../../lib/client";
+import { Product } from "../../components";
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
@@ -25,44 +26,55 @@ const ProductDetails = ({ product, products }) => {
                         ))}
                     </div> */}
                 </div>
+
                 <div className="product-detail-desc">
                     <h1>{name}</h1>
-                    <div>
-                        <div className="reviews">
-                            <div>
-                                <AiFillStar />
-                                <AiFillStar />
-                                <AiFillStar />
-                                <AiFillStar />
-                                <AiOutlineStar />
-                            </div>
-                            <p>(20)</p>
+
+                    <div className="reviews">
+                        <div>
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiOutlineStar />
                         </div>
-                        <h4>Details: </h4>
-                        <p>{details}</p>
-                        <p className="price">${price}</p>
-                        <div className="quantity">
-                            <h3>Quantity:</h3>
-                            <p className="quantity-desc">
-                                <span className="minus" onClick="">
-                                    <AiOutlineMinus />
-                                </span>
-                                <span className="num" onClick="">
-                                    0
-                                </span>
-                                <span className="plus" onClick="">
-                                    <AiOutlinePlus />
-                                </span>
-                            </p>
-                        </div>
-                        <div className="buttons">
-                            <button className="add-to-cart" type="button">
-                                Add to Cart
-                            </button>
-                            <button className="buy-now" type="button">
-                                Buy Now
-                            </button>
-                        </div>
+                        <p>(20)</p>
+                    </div>
+                    <h4>Details: </h4>
+                    <p>{details}</p>
+                    <p className="price">${price}</p>
+                    <div className="quantity">
+                        <h3>Quantity:</h3>
+                        <p className="quantity-desc">
+                            <span className="minus" onClick="">
+                                <AiOutlineMinus />
+                            </span>
+                            <span className="num" onClick="">
+                                0
+                            </span>
+                            <span className="plus" onClick="">
+                                <AiOutlinePlus />
+                            </span>
+                        </p>
+                    </div>
+                    <div className="buttons">
+                        <button className="add-to-cart" type="button">
+                            Add to Cart
+                        </button>
+                        <button className="buy-now" type="button">
+                            Buy Now
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="maylike-products-wrapper">
+                <h2>You may also like</h2>
+                <div className="marquee">
+                    <div className="maylike-products-container track">
+                        {products.map((item) => (
+                            <Product key={item._id} product={item} />
+                        ))}
                     </div>
                 </div>
             </div>
