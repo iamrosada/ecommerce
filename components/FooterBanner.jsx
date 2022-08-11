54905278e87e2c8e83084207e1d95b9653828f10
iamrosada <luisrosada2018@gmail.com>
2022-08-11 14:41:38 +0300

    feature: added th text in footbanner

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -3,8 +3,20 @@ import Link from "next/link";
 
 import { urlFor } from "../lib/client";
 
-const FooterBanner = ({ footBanner }) => {
-    return <div className="foot-banner-container">{}</div>;
+const FooterBanner = ({ footerBanner }) => {
+    return (
+        <div className="footer-banner-container">
+            <div className="banner-desc">
+                <div className="left">
+                    <p>{footerBanner.discount}</p>
+                    <h3>{footerBanner.largeText1}</h3>
+                    <h3>{footerBanner.largeText2}</h3>
+                    <p>{footerBanner.saleTime}</p>
+                </div>
+                <div className="right"></div>
+            </div>
+        </div>
+    );
 };
 
 export default FooterBanner;
