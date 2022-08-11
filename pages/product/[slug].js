dab84f2ada91db4c20929833a05ad88c82cc6cd8
iamrosada <luisrosada2018@gmail.com>
2022-08-11 19:31:25 +0300

    feature: added state to show the image the was selected

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -10,21 +10,28 @@ import { Product } from "../../components";
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
+    const [index, setIndex] = useState(0);
     return (
         <div>
             <div className="product-detail-container">
                 <div>
                     <div className="image-container">
-                        <img src={urlFor(image && image[0])} alt="" />
+                        <img src={urlFor(image && image[index])} alt="" />
                     </div>
-                    {/* <div className="small-images-container">
+                    <div className="small-images-container">
                         {image?.map((item, i) => (
-                            <img src={urlFor(item)} alt="" 
-                            className=""
-                            onMouseEnter=""
+                            <img
+                                src={urlFor(item)}
+                                alt=""
+                                className={
+                                    i === index
+                                        ? "small-image selected-image"
+                                        : "small-image"
+                                }
+                                onMouseEnter={() => setIndex(i)}
                             />
                         ))}
-                    </div> */}
+                    </div>
                 </div>
 
                 <div className="product-detail-desc">
