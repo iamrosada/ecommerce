84ae64b1e1c3294da5f0b3f28f8edc5adf5e0f74
iamrosada <luisrosada2018@gmail.com>
2022-08-11 14:32:48 +0300

    feature: starting to create the banner

---
+++ components/FooterBanner.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from "next/link";
+
+import { urlFor } from "../lib/client";
+
+const FooterBanner = ({ footBanner }) => {
+    return <div className="foot-banner-container">{}</div>;
+};
+
+export default FooterBanner;
