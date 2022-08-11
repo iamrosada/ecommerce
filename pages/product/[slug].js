a508754e9ee1d7cc28718b2be20217650827aee6
iamrosada <luisrosada2018@gmail.com>
2022-08-11 16:18:15 +0300

    feature: added th getStaticPaths to return the object that was selected

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,17 +2,37 @@ import React from "react";
 import { urlFor, client } from "../../lib/client";
 
 const ProductDetails = ({ product, products }) => {
+    const { image, name, details, price } = product;
     return (
         <div>
             <div className="product-detail-container">
                 <div>
                     <div className="image-container">
-                        <img src={urlFor()} alt="" />
+                        <img src={urlFor(image && image[0])} alt="" />
                     </div>
                 </div>
             </div>
         </div>
     );
+};
+
+export const getStaticPaths = async () => {
+    //para return os dados do produto clicado
+    const query = `*[_type== "product"]{
+      slug{
+        current
+      }
+    }`;
+    const products = await client.fetch(query);
+    const paths = produts.map((product) => ({
+        params: {
+            slug: product.slug.current,
+        },
+    }));
+    return {
+        paths,
+        fallback: "blocking",
+    };
 };
 export const getStaticProps = async ({ params: { slug } }) => {
     const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
