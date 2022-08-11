577676b7f17233fb807fb349e7eb81b0c0896402
iamrosada <luisrosada2018@gmail.com>
2022-08-11 13:36:19 +0300

    feature: added props to component HeroBanner from Sanity

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,14 @@ import { Product, FooterBanner, HeroBanner } from "../components";
 const Home = ({ products, bannerData }) => {
     return (
         <>
-            <HeroBanner />
-
+            <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
             <div className="products-heading">
                 <h2>Beset Selling Product</h2>
                 <p>Speakers of many variations</p>
             </div>
             <div className="products-container">
-                {["Product 1", "Product 2"].map((product) => {
-                    return product;
+                {products?.map((product) => {
+                    return product.name;
                 })}
             </div>
 
