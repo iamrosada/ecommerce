8245ae36fa49d81164dae94caed6060ad78d739e
iamrosada <luisrosada2018@gmail.com>
2022-08-11 20:02:16 +0300

    feature: created the context provider

--- context/StateContext.js
+++ context/StateContext.js
@@ -4,9 +4,23 @@ import { toast } from "react-hot-toast";
 const Context = createContext();
 
 export const StateContext = ({ children }) => {
-    const [show, setShow] = useState(false);
+    const [showCart, setShowCart] = useState(false);
     const [cartItems, setCartItems] = useState();
     const [totalPrice, setTotalPrice] = useState();
     const [totalQuantities, setTotalQuantities] = useState();
     const [qty, setQty] = useState(1);
+
+    return (
+        <Context.Provider
+            value={{
+                showCart,
+                cartItems,
+                totalPrice,
+                totalQuantities,
+                qty,
+            }}
+        >
+            {children}
+        </Context.Provider>
+    );
 };
