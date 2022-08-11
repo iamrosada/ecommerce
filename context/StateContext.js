8ec990456ff67916dde254c3b280194a52ea0766
iamrosada <luisrosada2018@gmail.com>
2022-08-11 20:06:47 +0300

    feature: created decrement function

--- context/StateContext.js
+++ context/StateContext.js
@@ -13,7 +13,12 @@ export const StateContext = ({ children }) => {
     const incQty = () => {
         setQty((prevQty) => prevQty + 1);
     };
-
+    const decQty = () => {
+        setQty((prevQty) => {
+            if (prevQty - 1 < 1) return 1;
+            return prevQty - 1;
+        });
+    };
     return (
         <Context.Provider
             value={{
