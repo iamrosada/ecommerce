b0ca40666a71ff8f61a569c826e64b61af2dfbbd
iamrosada <luisrosada2018@gmail.com>
2022-08-12 16:51:37 +0300

    feature: updated the navcard

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,27 @@
 import Link from "next/link";
 import React from "react";
 import { AiOutlineShopping } from "react-icons/ai";
+import { Cart } from "./";
+import { useStateContext } from "../context/StateContext";
 
 const Navbar = () => {
+    const { showCart, setShowCart, totalQuantities } = useStateContext();
     return (
         <div className="navbar-container">
             <p className="logo">
                 <Link href="/">Môbiolo Headphones</Link>
             </p>
-            <button className="cart-icon" type="button">
+            <button
+                className="cart-icon"
+                type="button"
+                onClick={() => {
+                    setShowCart(true);
+                }}
+            >
                 <AiOutlineShopping />
-                <span className="cart-item-qty">1</span>
+                <span className="cart-item-qty">{totalQuantities}</span>
             </button>
+            {showCart && <Cart />}
         </div>
     );
 };
