ad3fa1cfc804b6506e07bc8275e09c75c04d2b6d
iamrosada <luisrosada2018@gmail.com>
2022-08-12 17:02:57 +0300

    updated the main page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -12,7 +12,12 @@ import { useStateContext } from "../../context/StateContext";
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
     const [index, setIndex] = useState(0);
-    const { decQty, incQty, qty, onAdd } = useStateContext();
+    const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+
+    const handleBuyNow = () => {
+        onAdd(product, qty);
+        setShowCart(true);
+    };
     return (
         <div>
             <div className="product-detail-container">
@@ -63,9 +68,7 @@ const ProductDetails = ({ product, products }) => {
                             <span className="minus" onClick={decQty}>
                                 <AiOutlineMinus />
                             </span>
-                            <span className="num" onClick="">
-                                {qty}
-                            </span>
+                            <span className="num">{qty}</span>
                             <span className="plus" onClick={incQty}>
                                 <AiOutlinePlus />
                             </span>
@@ -79,7 +82,11 @@ const ProductDetails = ({ product, products }) => {
                         >
                             Add to Cart
                         </button>
-                        <button className="buy-now" type="button">
+                        <button
+                            className="buy-now"
+                            type="button"
+                            onClick={() => handleBuyNow}
+                        >
                             Buy Now
                         </button>
                     </div>
