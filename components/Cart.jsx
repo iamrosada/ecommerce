e69d9c13d5e65a5a31689fcd960aa1b8608affe1
iamrosada <luisrosada2018@gmail.com>
2022-08-12 16:52:00 +0300

    feature: added the stripe to Cart component

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -8,7 +8,7 @@ import {
 } from "react-icons/ai";
 import { TiDeleteOutline } from "react-icons/ti";
 import { toast } from "react-hot-toast";
-
+import getStripe from "../lib/getStripe";
 import { useStateContext } from "../context/StateContext";
 import { urlFor } from "../lib/client";
 
@@ -23,6 +23,25 @@ const Cart = () => {
         onRemove,
     } = useStateContext();
 
+    const handleCheckout = async () => {
+        const stripe = await getStripe();
+
+        const response = await fetch("/api/stripe", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(cartItems),
+        });
+
+        if (response.statusCode === 500) return;
+
+        const data = await response.json();
+
+        toast.loading("Redirecting...");
+
+        stripe.redirectToCheckout({ sessionId: data.id });
+    };
     return (
         <div className="cart-wrapper" ref={cartRef}>
             <div className="cart-container">
@@ -116,7 +135,11 @@ const Cart = () => {
                             <h3>${totalPrice}</h3>
                         </div>
                         <div className="btn-container">
-                            <button type="button" className="btn" onClick="">
+                            <button
+                                type="button"
+                                className="btn"
+                                onClick={handleCheckout}
+                            >
                                 Pay with Stripe
                             </button>
                         </div>
